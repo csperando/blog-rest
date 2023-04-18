86eea7e9a7a48633bec40d7d3648d593a5db8007
csperando <csperando@gmail.com>
2023-04-18 14:26:28 -0600

    dependencies

--- v1/app.ts
+++ v1/app.ts
@@ -1,20 +1,20 @@
 import express, { Request, Response } from "express";
-import cors, { CorsOptions } from "cors";
+// import cors, { CorsOptions } from "cors";
 
 const app = express();
 
-// CORS
-const whitelist: string[] = [];
-const setCorsOptions = function (req: Request, callback: any) {
-    let options = {} as CorsOptions;
+// // CORS
+// const whitelist: string[] = [];
+// const setCorsOptions = function (req: Request, callback: any) {
+//     let options = {} as CorsOptions;
 
-    if (whitelist?.indexOf(req.header("Origin") || 'localhost') !== -1) {
-        options.origin = req.header("Origin");
-    }
+//     if (whitelist?.indexOf(req.header("Origin") || 'localhost') !== -1) {
+//         options.origin = req.header("Origin");
+//     }
 
-    callback(null, options);
-};
-app.use(cors(setCorsOptions));
+//     callback(null, options);
+// };
+// app.use(cors(setCorsOptions));
 
 // router
 import router from "./routes";
--- v1/routes/ping.ts
+++ v1/routes/ping.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-// import { apiResponse } from "../models/apiResponse";
 
 const router = express.Router();
 
