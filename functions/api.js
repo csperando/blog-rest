d8ff9ac72b0e7a597d9a2271f53be6d89e991d1b
csperando <csperando@gmail.com>
2023-04-18 15:06:46 -0600

    test

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,12 @@
 const serverless = require("serverless-http");
 
-const express = require("express");
-const app = express();
-app.use("/", (req, res) => {
-	res.status(200);
-	res.send("Hello!");
-});
+// const express = require("express");
+// const app = express();
+// app.use("/", (req, res) => {
+// 	res.status(200);
+// 	res.send("Hello!");
+// });
+
+const app = require("../dist/app");
 
 module.exports.handler = serverless(app);
