44bd4fde5f6a445e8d7b545bc06638d9d1a0653c
csperando <csperando@gmail.com>
2023-04-18 14:38:37 -0600

    cleanup

--- v1/app.ts
+++ v1/app.ts
@@ -1,22 +1,7 @@
-import express, { Request, Response } from "express";
-// import cors, { CorsOptions } from "cors";
+import express from "express";
 
 const app = express();
 
-// // CORS
-// const whitelist: string[] = [];
-// const setCorsOptions = function (req: Request, callback: any) {
-//     let options = {} as CorsOptions;
-
-//     if (whitelist?.indexOf(req.header("Origin") || 'localhost') !== -1) {
-//         options.origin = req.header("Origin");
-//     }
-
-//     callback(null, options);
-// };
-// app.use(cors(setCorsOptions));
-
-// router
 import router from "./routes";
 router(app);
 
