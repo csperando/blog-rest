727c7f6387dc0809b12f0344c7a01484ef3c44f5
Coleman Sperando <csperando@gmail.com>
2024-01-05 11:44:24 -0500

    server up message

--- v1/server.ts
+++ v1/server.ts
@@ -4,7 +4,7 @@ import { config, setupDatabase } from "./config";
 setupDatabase()
 
 const server = app.listen(config.rest_port, () => {
-    console.log("server up on 3000");
+    console.log(`REST server up on port [${config.rest_port}]`);
 });
 
 export default server;
