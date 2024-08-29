c603869712c6f386845eb26fee6181560716935b
csperando <csperando@gmail.com>
2024-08-29 16:31:17 -0600

    hard code 8080 for azure

--- v1/server.ts
+++ v1/server.ts
@@ -4,8 +4,9 @@ import { config, setupDatabase } from "./config";
 
 setupDatabase();
 
-const server = app.listen(config.rest_port, () => {
-    console.log(`REST server up on port [${config.rest_port}]`);
+const port = config.rest_port || 8080;
+const server = app.listen(port, () => {
+    console.log(`REST server up on port [${port}]`);
 });
 
 export default server;
