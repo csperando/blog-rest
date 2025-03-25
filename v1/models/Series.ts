6802df175d73d0746ceea76731ea34d87a695ce5
Coleman Sperando <csperando@gmail.com>
2025-03-25 17:11:56 -0600

    create series model

--- v1/models/Series.ts
+++ v1/models/Series.ts
@@ -1,33 +1,30 @@
 import { NextFunction } from "express";
 import { Schema, model } from "mongoose";
+import { blogPostSchema, iBlogPost } from "./Blog";
 
-export interface iBlogPost {
+export interface iSeries {
     title: string;
-    author: string;
-    author_id: string;
     slug?: string;
     description: string;
     keywords: string[];
-    markdown: string;
-    html: string;
+    posts: [iBlogPost]
     thumbnail?: string;
     mime?: string;
+    latestPost?: String;
     created?: string;
     updated?: string;
 }
 
-export const blogPostSchema = new Schema<iBlogPost>(
+export const seriesSchema = new Schema<iSeries>(
     {
         title: { type: String, required: true },
-        author: { type: String, required: true },
-        author_id: { type: String, required: true },
         slug: { type: String, required: false },
         description: String,
         keywords: [String],
-        markdown: String,
-        html: String,
+        posts: [blogPostSchema],
         thumbnail: String,
         mime: String,
+        latestPost: String,
         created: String,
         updated: String,
     },
@@ -42,30 +39,19 @@ export const blogPostSchema = new Schema<iBlogPost>(
 // update slug on save and update
 
 // when using 'save' this is the document
-blogPostSchema.pre("save", function(): void {
+seriesSchema.pre("save", function(): void {
     let slug: string = this.title.trim().toLowerCase();
-    
-    // 'error: replaceAll does not exist on type string' ???
-    while(slug.indexOf(" ") != -1) {
-        slug = slug.replace(" ", "-");
-    }
-
+    this.slug = slug.replace(/ /g, "-");
     this.slug = slug;
 });
 
 // When using 'findOneAndUpdate' this is the query
-blogPostSchema.pre('findOneAndUpdate', function(): void {
+seriesSchema.pre('findOneAndUpdate', function(): void {
     const doc = this.getUpdate() as iBlogPost;
-
     if(!doc.slug) {
         doc.slug = doc.title.trim().toLowerCase().replace(/ /g, "-");
         this.setUpdate(doc);
     }
 });
 
-export const BlogPost = model<iBlogPost>("BlogPost", blogPostSchema);
-
-
-// View models
-
-export const TopKeywords = model<iBlogPost>("TopKeywords", blogPostSchema);
+export const Series = model<iSeries>("Series", seriesSchema);
